aae65fa0c405dc639c28710cc2255441f828705a
Ravena73 <00001120331766sp@al.educacao.sp.gov.br>
2024-11-26 05:28:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Física',
+    'O que é força gravitacional',
+    'É a força de atração que ocorre entre dois corpos devido às suas massas'
+)
+
+criaCartao(
+    'Química',
+    'O que é uma solução',
+    'É uma mistura homogênea formada por um soluto dissolvido em um solvente'
+)
+
+criaCartao(
+    'Biologia',
+    'O que são os seres autotróficos',
+    'São seres que produzem seu próprio alimento por meio da fotossíntese ou quimiossíntese'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é um número racional',
+    'É um número que pode ser representado como a razão entre dois números inteiros onde o denominador é diferente de zero'
+)
